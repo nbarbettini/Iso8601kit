84fe9d15b523554947b1b90f13ddd55fafb6f6cc
Nate Barbettini <nathanaelb@gmail.com>
2015-12-14 14:18:00 -0800

    Initialize proj

---
+++ SimpleDurations/SimpleDurations.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Nate Barbettini">
+// Copyright (c) 2015. Licensed under MIT.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleDurations.Tests")]
+[assembly: AssemblyDescription("Simple ISO 8601 durations library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nate Barbettini")]
+[assembly: AssemblyProduct("SimpleDurations")]
+[assembly: AssemblyCopyright("Copyright © Nate Barbettini 2015. License: MIT")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("92e8ec6c-139d-480c-82ea-ed630e00d8b6")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleDurations/SimpleDurations/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Nate Barbettini">
+// Copyright (c) 2015. Licensed under MIT.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleDurations")]
+[assembly: AssemblyDescription("Simple ISO 8601 durations library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nate Barbettini")]
+[assembly: AssemblyProduct("SimpleDurations")]
+[assembly: AssemblyCopyright("Copyright © Nate Barbettini 2015. License: MIT")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("bae7883b-afc5-438b-9aff-569ea904487b")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
